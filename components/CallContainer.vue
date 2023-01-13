9a9ee8d3cedca7baa7cabac2d6f733217d615c2b
jason <pnxl@pm.me>
2023-01-14 00:52:53 +0700

    remove hardcoded shit

--- components/CallContainer.vue
+++ components/CallContainer.vue
@@ -5,17 +5,19 @@
         class="flex bg-neutral-800 p-3 rounded-full w-2/3 shadow-lg justify-between"
       >
         <div class="flex gap-x-4">
-          <img
-            src="https://fangdustry.me/assets/jayden.c6f75387.png"
-            class="h-14 w-14 m-0 rounded-full my-auto"
-          />
+          <img :src="img" class="h-14 w-14 m-0 rounded-full my-auto" />
           <div class="flex flex-col h-full my-auto justify-center">
-            <span class="text-neutral-400 font-light text-sm">iPhone (Cellular)</span>
-            <strong class="-my-0.5">Sam — 02:27</strong>
+            <div class="flex text-neutral-400 gap-x-1.5">
+              <i :class="`fa-solid fa-${icon} text-sm`"></i
+              ><span class="font-light text-sm my-auto">{{ platform }}</span>
+            </div>
+            <strong class="-my-0.5">{{ contact }} — {{ duration }}</strong>
           </div>
         </div>
         <div class="flex gap-x-4">
-          <div class="bg-neutral-700 rounded-full h-14 w-14 flex justify-center">
+          <div
+            class="bg-neutral-700 rounded-full h-14 w-14 flex justify-center"
+          >
             <div class="flex flex-col justify-center">
               <i class="fa-solid fa-volume-high text-2xl text-white"></i>
             </div>
@@ -33,7 +35,15 @@
         </div>
       </div>
     </div>
-    <slot />
+    <span class="-my-0.5 text-neutral-400 text-center"
+      >Call transcription started at {{ transcribeStart }}.</span
+    >
+    <div class="flex gap-y-4 flex-col">
+      <slot />
+    </div>
+    <span class="-my-0.5 text-neutral-400 text-center"
+      >Call ended at {{ transcribeEnd }}.</span
+    >
   </div>
 </template>
 
@@ -41,7 +51,12 @@
 export default {
   props: {
     platform: String,
-    time: String,
+    icon: String,
+    contact: String,
+    duration: String,
+    img: String,
+    transcribeStart: String,
+    transcribeEnd: String,
   },
 };
 </script>
