44145a69d51da9330f4aa645b99da385e5954373
jason <development@pnxl.dev>
2023-11-05 18:21:58 +0700

    link to page

---
+++ components/content/LinkToPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex gap-x-1 -my-[0.0625rem]">
+    <a :href="link" class="dark:text-sky-300 text-sky-600">
+      {{ text }}
+    </a>
+    <i
+      class="fa-solid fa-link text-sm my-auto text-gray-600 dark:text-gray-500"
+    ></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    link: String,
+    text: String,
+  },
+};
+</script>
